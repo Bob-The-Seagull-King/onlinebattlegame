46ca2d63d11171e45a1fbdae8be1adc2b62bdc6a
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-08-21 13:14:09 +1200

    team factory

---
+++ src/classes/sim/factories/team_factory.ts
@@ -0,0 +1,22 @@
+import { IDEntry } from "../../../global_types";
+import { ITeam, Team } from "../models/team";
+
+class TeamFactory {
+
+    public static CreateTeam(_team : ITeam) {
+        const newTeam = new Team(_team);
+        return newTeam;
+    }
+
+    public static CreateNewTeam(_team : IDEntry) {
+        const freshTeam : ITeam = {
+            items: [],
+            monsters: [],
+            active: []
+        }
+        return TeamFactory.CreateTeam(freshTeam);
+    }
+
+}
+
+export {TeamFactory}
