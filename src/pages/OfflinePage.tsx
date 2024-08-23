53c212835960c434354dd856005573901cf774e0
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-08-23 15:30:14 +1200

    incomplete - test page setup

--- src/pages/OfflinePage.tsx
+++ src/pages/OfflinePage.tsx
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import '../resources/styles/App.css';
+import '../resources/styles/CustomStyleHost.scss'
 import { OfflineBattleManager } from '../classes/viewmodel/battle_manager_local';
 import MessagesDisplay from './Screen/MessagesDisplay';
 import OptionsDisplay from './Screen/OptionsDisplay';
+import Button from 'react-bootstrap/Button';
 
 const OfflinePage = (props: any) => {
 
@@ -18,9 +20,24 @@ const OfflinePage = (props: any) => {
   // DOM Return
   return (
     <div className="App">
-      <button onClick={joinOfflineBattle}> Join Offline Battle</button>
-      <MessagesDisplay manager={battleManager}/>
-      <OptionsDisplay manager={battleManager}/>
+      <div className="row">
+        <div className="col-12">
+          <Button onClick={joinOfflineBattle} size="lg"> Join Offline Battle</Button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-4">
+          <div className="BasicElementContainer CapHeight80">
+            <OptionsDisplay manager={battleManager}/>
+          </div>
+        </div>
+        <div className="col-8">
+          <div className="BasicElementContainer CapHeight80">
+            <MessagesDisplay manager={battleManager}/>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
