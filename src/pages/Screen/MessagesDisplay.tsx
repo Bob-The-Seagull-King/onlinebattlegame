9f065455fa3a4ed586fe9faaf9dad5689a2400ae
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-08-28 08:13:03 +1200

    Documentation screens

--- src/pages/Screen/MessagesDisplay.tsx
+++ src/pages/Screen/MessagesDisplay.tsx
@@ -1,42 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
-import logo from '../resources/images/npm install socket.io-client.svg';
 import '../../resources/styles/App.css';
-import * as io from "socket.io-client";
-import { useEffect, useState } from "react";
-import { SocketManager } from '../../classes/structure/connection/SocketManager';
-import { OnlineBattleManager } from '../../classes/viewmodel/battle_manager_online';
-import { OfflineBattleManager } from '../../classes/viewmodel/battle_manager_local';
+import { useState } from "react";
 import { BattleManager } from '../../classes/viewmodel/battle_manager';
 
 const MessagesDisplay = (props: any) => {
-  const Manager : BattleManager = props.manager;
-  // Messages States
+  const Manager : BattleManager = props.manager; // The manager running this battle
+  
+  // Collection of messages to displpay
   const [messageReceived, setMessageReceived] = useState([]);
   
+  // Update the state of messages to match the manager
   const receiveMessage = () => {
     const CurrentMessages =  Object.assign([], Manager.MessageLog);
     setMessageReceived(CurrentMessages);
   }
   
+  // Assign the relevant function to the manager
   Manager.setResultFuncs(receiveMessage)
   
-  // DOM Return
   return (
-    <div className="">
+    <div>
       <h1 className="BigText"> BATTLE MESSAGES</h1>
+      
       <div className="BasicElementContainer overflow-auto flex-grow-1">
         <div className="ForceHeight50">
+            {/** Display each message in human readable format */}
             {messageReceived.map(item => (
               <div key={"messageset"+messageReceived.indexOf(item)}>
                 <p>{"Message Set " + messageReceived.indexOf(item)}</p>
                 {Manager.TranslateMessages(item).map(element => (
-                  <p key={"messageofset"+messageReceived.indexOf(item)+(element)}>
-                    {element}
-                  </p>
-                ))}
-              </div>
-            ))}
+                  <p key={"messageofset"+messageReceived.indexOf(item)+(element)}> {element} </p> ))}
+              </div> ))}
           </div>
       </div>
     </div>
--- src/pages/Screen/OptionsDisplay.tsx
+++ src/pages/Screen/OptionsDisplay.tsx
@@ -1,39 +1,38 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import '../../resources/styles/App.css';
-import * as io from "socket.io-client";
 import { useState } from "react";
 import { BattleManager } from '../../classes/viewmodel/battle_manager';
-import { SelectedAction } from '../../global_types';
 import OptionsMonsterDisplay from '../Components/Options/OptionMonsterDisplay';
 
 const OptionsDisplay = (props: any) => {
-  const Manager : BattleManager = props.manager;
-  // Messages States
-
+  const Manager : BattleManager = props.manager; // The manager running this battle
+ 
+  // Collection of options available for the user to choose from
   const [optionsReceived, setOptionsReceived] = useState(Manager.ChoicesLog);
   
-
+  // Update the state of options to match the manager
   const receiveOptions = () => {
     const options =  Object.assign([], Manager.ChoicesLog);
     setOptionsReceived(options);
   }
 
+  // Assign the relevant function to the manager
   Manager.setOptionsFuncs(receiveOptions)
   
-  // DOM Return
   return (
     <div>      
       <h1 className="BigText"> BATTLE OPTIONS</h1>
+
       <div className="BasicElementContainer overflow-auto">
         <div className='row justify-content-center'>
           <div className='col-11'>
             <div className="ForceHeight50" style={{width:"100%", justifyContent:"center"}}>
+              {/** Display each suite of choices */}
               {optionsReceived.map(item => (
                 <div key={"choice" + optionsReceived.indexOf(item)}>
                   <OptionsMonsterDisplay manager={Manager} turns={item.action} position={item.pos}/>
-                </div>
-              )) }
+                </div> ))}
             </div>
           </div>
         </div>
