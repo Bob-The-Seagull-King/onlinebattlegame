5afe6a233a74768cd7a28bb99814462b961a11ae
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-08-27 22:01:44 +1200

    Documentation target choices

--- src/pages/Components/Options/Choices/ItemChoicesDisplay.tsx
+++ src/pages/Components/Options/Choices/ItemChoicesDisplay.tsx
@@ -1,19 +1,20 @@
 import { Button, Collapse } from "react-bootstrap";
 import { BattleManager } from "../../../../classes/viewmodel/battle_manager";
-import { SelectedAction, SubSelectAction, SwitchAction } from "../../../../global_types";
-import { MessageTranslator } from "../../../../classes/tools/translator";
+import { SelectedAction, SubSelectAction } from "../../../../global_types";
 import { useState } from "react";
 import { ItemInfoDex } from "../../../../data/static/item/item_inf";
 import { ActiveItem } from "../../../../classes/sim/models/active_item";
 import { ActionTranslateDex } from "../../../../classes/tools/translator_static";
 
 const ItemChoicesDisplay = (props: any) => {
-    const Manager : BattleManager = props.manager;
-    const Choices : SubSelectAction = props.turns;
-    const Position : number = props.position
+    const Manager   : BattleManager = props.manager;    // The viewmodel manager object
+    const Choices   : SubSelectAction = props.turns;    // The list of choices being displayed
+    const Position  : number = props.position           // The ID val of this set of choices (used for when multiple pokemon are on the field)
 
+    // Determines if the item shows all possible sub items
     const [open, setOpen]   = useState(false);
 
+    // Sends a selected objct to the viewmodel manager as the chosen action
     const SendSingleOption = (_item : SelectedAction, _pos : number) => {
         Manager.SendOptions(_item, _pos)
     }
