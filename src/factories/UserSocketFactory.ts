8aa1aaabe0ea73e539fd297d02276dbad0721a4e
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-08-27 19:49:17 +1200

    Documentation - UserSocketFactory

--- src/factories/UserSocketFactory.ts
+++ src/factories/UserSocketFactory.ts
@@ -1,10 +1,17 @@
 import { SocketHold } from "../classes/structure/socket/SocketHold";
 import { ServerHold } from "../classes/structure/server/ServerHold";
-import { UserHold, IUserConstruct } from "../classes/structure/user/UserHold";
+import { UserHold } from "../classes/structure/user/UserHold";
 import { UserStore } from "../classes/structure/user/UserStore";
 
 class UserSocketFactory {
 
+    /**
+     * Create a User and Socket object in response to a
+     * socket connecting to the Server.
+     * @param socket the newly connected socket
+     * @param server the server hosting the socket
+     * @param userstore the store holding user objects
+     */
     public static BuildUserAndSocket(socket : any, server : ServerHold, userstore : UserStore) {
 
         const newsocket = new SocketHold(socket, server);
