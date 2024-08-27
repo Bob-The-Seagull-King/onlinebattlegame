5e77444a08f7db27fd0200add898fec4e33a7b24
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-08-27 15:10:28 +1200

    Documentation - User

--- src/classes/structure/user/UserHold.ts
+++ src/classes/structure/user/UserHold.ts
@@ -1,25 +1,36 @@
 import { SocketHold } from "../socket/SocketHold";
 import { UserStore } from "./UserStore";
 
+/**
+ * Interface of the user constructor
+ */
 interface IUserConstruct {
-    store: UserStore;
-    name?: String;
-    type?: Number;
+    store   : UserStore;   // The storage which holds this User
+    name?   : String;      // The name of the User
+    type?   : Number;      // The User's type
 }
 
 class UserHold {
 
-    MySocket: SocketHold;
-    Name: String;
-    Type: Number;
-    Store: UserStore;
+    MySocket    : SocketHold;   // The socket associated with this user
+    Name        : String;       // The name of the User
+    Type        : Number;       // The User's type
+    Store       : UserStore;    // The sotrage which holds this User
 
+    /**
+     * Simple constructor
+     * @param _user the interface of User information
+     */
     constructor(_user : IUserConstruct) {
         this.Store = _user.store;
         this.Type = _user.type? _user.type : 0;
         this.Name = _user.name? _user.name : this.Store.GenerateName();
     }
 
+    /**
+     * Sets the User's socket to a given object
+     * @param _socket the socket to associated with
+     */
     public AssignSocket(_socket : SocketHold) {
         this.MySocket = _socket;
     }
--- src/classes/structure/user/UserStore.ts
+++ src/classes/structure/user/UserStore.ts
@@ -3,21 +3,37 @@ import { ServerHold } from "../server/ServerHold";
 
 class UserStore {
 
-    CurrentUsers: UserHold[] = [];
-    MyServer: ServerHold;
+    CurrentUsers    : UserHold[] = [];  // Collection of all Users
+    MyServer        : ServerHold;       // The server this store contains information for
 
+    /**
+     * Simple constructor
+     * @param _server the Server which this store exists for
+     */
     constructor(_server : ServerHold) {
         this.MyServer = _server;
     }
 
+    /**
+     * Creates a simple identifier for a User
+     * @returns string new name of a User
+     */
     public GenerateName() {
         return "Anonymous" + this.CurrentUsers.length.toString();
     }
 
+    /**
+     * Given a user, add it to the storage
+     * @param _user the User to add
+     */
     public AddUser(_user : UserHold) {
         this.CurrentUsers.push(_user);
     }
 
+    /**
+     * Given a user, discard it from this storage collection
+     * @param _user the User to remove
+     */
     public RemoveUser(_user : UserHold) {
         const userIndex = this.CurrentUsers.indexOf(_user);
         this.CurrentUsers.splice(userIndex, 1);
