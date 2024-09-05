6f384c8a16a8ba1962364ac66a063b4d02404185
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-09-05 17:12:42 +1200

    temp new teams

--- src/classes/structure/connection/SocketManager.ts
+++ src/classes/structure/connection/SocketManager.ts
@@ -67,6 +67,48 @@ class SocketManager {
     private TempNewTeam() : Team {
         const _Team : Team = TeamFactory.CreateNewTeam();
 
+        _Team.AddFreshItem('blueherb')
+        _Team.AddFreshItem('greenherb')
+        _Team.AddFreshItem('savouryberry')
+        _Team.AddFreshItem('strongsoil')
+
+        _Team.AddFreshMonster('marrowdread')
+        _Team.Monsters[0].AddFreshAction('sparkup')
+        _Team.Monsters[0].AddFreshAction('nausea')
+        _Team.Monsters[0].AddFreshAction('braindrain')
+        _Team.Monsters[0].AddFreshAction('stinger')
+        _Team.Monsters[0].Traits.push('vampire')
+
+        _Team.AddFreshMonster('humbood')
+        _Team.Monsters[1].AddFreshAction('deeproots')
+        _Team.Monsters[1].AddFreshAction('flytrap')
+        _Team.Monsters[1].AddFreshAction('pressurecannon')
+        _Team.Monsters[1].AddFreshAction('rockthrow')
+        _Team.Monsters[1].Traits.push('firstdefense')
+
+        _Team.AddFreshMonster('stalagmitendon')
+        _Team.Monsters[2].AddFreshAction('superhotslam')
+        _Team.Monsters[2].AddFreshAction('scatter')
+        _Team.Monsters[2].AddFreshAction('rockthrow')
+        _Team.Monsters[2].AddFreshAction('regrow')
+        _Team.Monsters[2].Traits.push('rockycomposition')
+
+        _Team.AddFreshMonster('impound')
+        _Team.Monsters[3].AddFreshAction('harshthenoise')
+        _Team.Monsters[3].AddFreshAction('flytrap')
+        _Team.Monsters[3].AddFreshAction('mindread')
+        _Team.Monsters[3].AddFreshAction('slam')
+        _Team.Monsters[3].Traits.push('sacrificialaltar')
+
+        _Team.AddFreshMonster('stratate')
+        _Team.Monsters[4].AddFreshAction('stormwinds')
+        _Team.Monsters[4].AddFreshAction('raindance')
+        _Team.Monsters[4].AddFreshAction('blindinglight')
+        _Team.Monsters[4].AddFreshAction('slam')
+        _Team.Monsters[4].Traits.push('scaryface')
+
+        _Team.Leads.push(new ActivePos(0,0,_Team));
+
         return _Team;
     }
 
