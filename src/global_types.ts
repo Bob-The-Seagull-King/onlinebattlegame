41372f73fcbf4dc31c1148fa63ef1ae445ba348d
Bob-The-Seagull-King <laneedwardsbrown@gmail.com>
2024-10-14 15:21:26 +1300

    Choice Target data type update

--- src/global_types.ts
+++ src/global_types.ts
@@ -265,6 +265,7 @@ export interface ChoiceTarget {
     target_pos          : "ALL" | "SINGLE" | "SMALL" | "MEDIUM" | "LARGE" ,    // If something a single space, or a blast area
     target_type         : "ALL" | "TERRAIN" | "MONSTER", // If something targets the terrain, the monster on it, or both
     target_direction    : "ALL" | "CARDINAL" | "ORDINAL" | "BOTH", // If the possible range is X, +, both, or anywhere within range
+    target_choice       : "ALL" | "TERRAIN" | "MONSTER", // If the player can select a space with a monster on it, a space without a monster on it, or either
     target_range        : number // how many spaces from the source it can be
 }
 
